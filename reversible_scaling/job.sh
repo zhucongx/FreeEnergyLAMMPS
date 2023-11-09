c8c997837d6d52d3d447ab96e83b6f171a2043cf
Zhucong Xi <43595065+zhucongx@users.noreply.github.com>
2023-11-08 21:05:57 -0500

    my test

--- reversible_scaling/job.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# This script executes a single LAMMPS simulations.
-
-lammps="../../lammps/src/lmp_serial" # Path to LAMMPS executable.
-
-mkdir -p data # Create directory structure for data output.
-
-# Run job.
-${lammps} -in in.lmp -log data/lammps.log -screen none -var RANDOM ${RANDOM}
